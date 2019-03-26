fbf33874d8aedbbad2e2e08e4f2f5803c54bcd5e
duanfuqiang1128 <44457606+duanfuqiang1128@users.noreply.github.com>
2019-03-26 12:35:18 +0800

    Add files via upload

--- 网易云音乐格式转换/ncm_switch.py
+++ 网易云音乐格式转换/ncm_switch.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import re
-
 def file_switch(path):
 	file_array = []
 	files= os.listdir(path)
@@ -9,13 +8,13 @@ def file_switch(path):
 		file_path = path + '\\' + file
 		if os.path.isfile(file_path):
 			if(type(file) == str):
-				if(re.match(r'.*ncm$', file, flags=0)):
+				if(re.match(r'.*ncm$', file,flags=0)):
 					file_array.append(file_path)
 		elif os.path.isdir(file_path):
 			file = str(file)
 			file_switch(f'{path}\\{file}')
 	for i in file_array:
-		now_path = os.path.dirname(os.path.realpath(sys.executable))
+		now_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 		os.system(f'{now_path}\\main.exe "{i}"')
 		new_file1 = i[:-3] + 'mp3'
 		new_file2 = i[:-3] + 'flac'
@@ -23,18 +22,17 @@ def file_switch(path):
 			os.system(f'del "{i}"')
 		else:
 			print(f'文件:{i} 转换失败!')
-
 if __name__ == "__main__":
-        print('请输入要转换的文件夹路径：')
-        path = input()
-        if(path):
-                if ((os.path.exists(path))):
-                        try:
-                                file_switch(path)
-                        except Exception as e:
-                                print('路径有误，请重试！')
-                else :
-                        print('路径不存在！')
-        else :
-                print("未输入要转换的文件夹！请重试！")
-        input()
+	print('请输入要转换的文件夹路径：')
+	path = input()
+	if(path):
+		if ((os.path.exists(path))):
+			try:
+				file_switch(path)
+			except Exception as e:
+				print('路径有误，请重试！')
+		else :
+			print('路径不存在！')
+	else :
+		print("未输入要转换的文件夹！请重试！")
+	input('全部转换完成！按任意键退出程序...')
